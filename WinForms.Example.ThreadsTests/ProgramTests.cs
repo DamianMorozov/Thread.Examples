b0ff5f7344cb06a45d3576be898bad4e9075531c
Morozov Damian <damian@morozov33.ru>
2020-06-21 20:51:00 +0300

    Updated WinForms.Example.ThreadsTests project

---
+++ WinForms.Example.ThreadsTests/ProgramTests.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+using NUnit.Framework;
+using WinForms.Example.Threads;
+
+namespace WinForms.Example.ThreadsTests
+{
+    public class ProgramTests
+    {
+        #region Private fields and properties
+
+        //
+
+        #endregion
+
+        /// <summary>
+        /// Setup private fields.
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+            TestContext.WriteLine(@"--------------------------------------------------------------------------------");
+            TestContext.WriteLine($@"{nameof(Setup)} start.");
+
+            //
+
+            TestContext.WriteLine($@"{nameof(Setup)} complete.");
+        }
+
+        /// <summary>
+        /// Reset private fields to default state.
+        /// </summary>
+        [TearDown]
+        public void Teardown()
+        {
+            TestContext.WriteLine(@"--------------------------------------------------------------------------------");
+            TestContext.WriteLine($@"{nameof(Teardown)} start.");
+            //
+            TestContext.WriteLine($@"{nameof(Teardown)} complete.");
+            TestContext.WriteLine(@"--------------------------------------------------------------------------------");
+        }
+
+        [Test]
+        public void Main_Execute_NoException()
+        {
+            TestContext.WriteLine(@"--------------------------------------------------------------------------------");
+            TestContext.WriteLine($@"{nameof(Main_Execute_NoException)} start.");
+            var sw = Stopwatch.StartNew();
+
+            Assert.DoesNotThrow(() => Program.Main(new[] {"-skip"}));
+            
+            sw.Stop();
+            TestContext.WriteLine($@"{nameof(Main_Execute_NoException)} complete. Elapsed time: {sw.Elapsed}");
+        }
+    }
+}
