18ba8c39f6a3eafd7cb7f1017dc38f8d926443e0
Morozov Damian <damian@morozov33.ru>
2020-06-21 20:50:47 +0300

    Updated WinForms.Example.Threads project

---
+++ WinForms.Example.Threads/FormMain.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace WinForms.Example.Threads
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WinForms.Example.Threads/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+
+[assembly: InternalsVisibleTo("WinForms.Example.ThreadsTests")]
+namespace WinForms.Example.Threads
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        internal static void Main(string[] args = null)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            if (args == null || !args.Contains("-skip"))
+                Application.Run(new FormMain());
+        }
+    }
+}
