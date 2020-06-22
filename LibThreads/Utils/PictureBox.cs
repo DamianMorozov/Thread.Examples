61e4a6ffc4e45f366632c06ee9b015737c19ea71
Morozov Damian <damian@morozov33.ru>
2020-06-22 15:58:34 +0300

    Added PictureBox.cs

---
+++ LibThreads/Utils/PictureBox.cs
@@ -0,0 +1,100 @@
+﻿using System.Threading.Tasks;
+
+namespace LibThreads.Utils
+{
+    public static class PictureBox
+    {
+        public static class Properties
+        {
+            public static class SetBitmap
+            {
+                private delegate void SetDelegate(System.Windows.Forms.PictureBox item, System.Drawing.Bitmap value);
+
+                private static void SetWork(System.Windows.Forms.PictureBox item, System.Drawing.Bitmap value)
+                {
+                    item.Image = value;
+                }
+
+                public static Task Async(System.Windows.Forms.PictureBox item, System.Drawing.Bitmap value)
+                {
+                    return Task.Run(() => Sync(item, value));
+                }
+
+                public static void Sync(System.Windows.Forms.PictureBox item, System.Drawing.Bitmap value)
+                {
+                    if (item != null)
+                    {
+                        if (item.InvokeRequired)
+                        {
+                            item.BeginInvoke(new SetDelegate(SetWork), item, value);
+                        }
+                        else
+                        {
+                            SetWork(item, value);
+                        }
+                    }
+                }
+            }
+
+            public static class SetImage
+            {
+                private delegate void SetDelegate(System.Windows.Forms.PictureBox item, System.Drawing.Image value);
+
+                private static void SetWork(System.Windows.Forms.PictureBox item, System.Drawing.Image value)
+                {
+                    item.Image = value;
+                }
+
+                public static Task Async(System.Windows.Forms.PictureBox item, System.Drawing.Image value)
+                {
+                    return Task.Run(() => Sync(item, value));
+                }
+
+                public static void Sync(System.Windows.Forms.PictureBox item, System.Drawing.Image value)
+                {
+                    if (item != null)
+                    {
+                        if (item.InvokeRequired)
+                        {
+                            item.BeginInvoke(new SetDelegate(SetWork), item, value);
+                        }
+                        else
+                        {
+                            SetWork(item, value);
+                        }
+                    }
+                }
+            }
+
+            public static class SetBackgroundImage
+            {
+                private delegate void SetDelegate(System.Windows.Forms.PictureBox item, System.Drawing.Image value);
+
+                private static void SetWork(System.Windows.Forms.PictureBox item, System.Drawing.Image value)
+                {
+                    item.BackgroundImage = value;
+                }
+
+                public static Task Async(System.Windows.Forms.PictureBox item, System.Drawing.Image value)
+                {
+                    return Task.Run(() => Sync(item, value));
+                }
+
+                public static void Sync(System.Windows.Forms.PictureBox item, System.Drawing.Image value)
+                {
+                    if (item != null)
+                    {
+                        if (item.InvokeRequired)
+                        {
+                            item.BeginInvoke(new SetDelegate(SetWork), item, value);
+                        }
+                        else
+                        {
+                            SetWork(item, value);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
