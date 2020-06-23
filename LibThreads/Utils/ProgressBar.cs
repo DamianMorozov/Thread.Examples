acf6e0a678e80ea364761505168413bb0958fe43
Morozov Damian <damian@morozov33.ru>
2020-06-23 14:01:33 +0300

    Updated LibThreads\Utils\ProgressBar.cs

--- LibThreads/Utils/ProgressBar.cs
+++ LibThreads/Utils/ProgressBar.cs
@@ -43,6 +43,78 @@ namespace LibThreads.Utils
                     }
                 }
             }
+
+            public static class SetMinimum
+            {
+                private delegate void SetDelegate(System.Windows.Forms.ProgressBar item, int value);
+
+                private static void SetWork(System.Windows.Forms.ProgressBar item, int value)
+                {
+                    item.Minimum = value;
+                }
+
+                public static Task Async(System.Windows.Forms.ProgressBar item, int value, int sleepTimeOutMs = 10)
+                {
+                    return Task.Run(() =>
+                    {
+                        Sync(item, value);
+                        Task.Delay(sleepTimeOutMs);
+                    });
+                }
+
+                public static void Sync(System.Windows.Forms.ProgressBar item, int value, int sleepTimeOutMs = 10)
+                {
+                    if (item != null)
+                    {
+                        if (item.InvokeRequired)
+                        {
+                            item.BeginInvoke(new SetDelegate(SetWork), item, value);
+                        }
+                        else
+                        {
+                            SetWork(item, value);
+                            System.Windows.Forms.Application.DoEvents();
+                            System.Threading.Thread.Sleep(sleepTimeOutMs);
+                        }
+                    }
+                }
+            }
+
+            public static class SetMaximum
+            {
+                private delegate void SetDelegate(System.Windows.Forms.ProgressBar item, int value);
+
+                private static void SetWork(System.Windows.Forms.ProgressBar item, int value)
+                {
+                    item.Maximum = value;
+                }
+
+                public static Task Async(System.Windows.Forms.ProgressBar item, int value, int sleepTimeOutMs = 10)
+                {
+                    return Task.Run(() =>
+                    {
+                        Sync(item, value);
+                        Task.Delay(sleepTimeOutMs);
+                    });
+                }
+
+                public static void Sync(System.Windows.Forms.ProgressBar item, int value, int sleepTimeOutMs = 10)
+                {
+                    if (item != null)
+                    {
+                        if (item.InvokeRequired)
+                        {
+                            item.BeginInvoke(new SetDelegate(SetWork), item, value);
+                        }
+                        else
+                        {
+                            SetWork(item, value);
+                            System.Windows.Forms.Application.DoEvents();
+                            System.Threading.Thread.Sleep(sleepTimeOutMs);
+                        }
+                    }
+                }
+            }
         }
     }
 
